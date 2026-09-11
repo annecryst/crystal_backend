145544af5be81e450be1787f027d92ba98bae651
MR-JLTC <mr-jltc@gmail.com>
2026-09-11 16:28:39 +0800

    allowed local ip

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: (origin, callback) => {
+      // Allow any origin (including your local IPs like 192.168.x.x) for development
+      callback(null, true); 
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
